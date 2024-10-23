466cf76247464c45b7752beeb2d73121d8eed078
Barnabas Chun <28585999+BarnabasChun@users.noreply.github.com>
2024-10-23 14:25:50 -0400

    Move folders under src

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import createJiti from 'jiti';
 const jiti = createJiti(fileURLToPath(import.meta.url));
 
 // Import env here to validate during build. Using jiti we can import .ts files :)
-jiti('./lib/env');
+jiti('./src/lib/env');
 
 import type { NextConfig } from 'next';
 
